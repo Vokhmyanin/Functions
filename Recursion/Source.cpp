f003f40c5c46b3cd96e05eb2296d9900f9a3cf4f
Евгений <@LAPTOP-IEGUVAC3>
2023-06-23 22:36:06 +0700

    Power&Fact DONE

--- Recursion/Source.cpp
+++ Recursion/Source.cpp
@@ -1,14 +1,49 @@
 #include <iostream>
 using namespace std;
 void elevator(int floor);
+int factorial(int n);
+double power(int a, int n);
+void fibo(long long int n, long long int a=0, long long int b=1);
+
+//#define ELEVATOR
+//#define FACT
+//#define POWER
+#define FIBO
+#define FIBO_2
 void main()
 {
 	setlocale(LC_ALL, "");
-	//cout << "Hello, Recursion";
+#ifdef ELEVATOR
 	int floor;
 	cout << "Введите номер этажа: "; cin >> floor;
 	elevator(floor);
+#endif
+
+#ifdef FACT
+int n;
+ cout << "Введите число: "; cin >> n;
+	cout << factorial(n) << endl;
+
+#endif
+#ifdef POWER
+int a; //основание
+int n; //показатель
+cout << "Введите основание и показатель степени "; cin >> a >> n;
+cout << power(a, n) << endl;
+#endif
+#ifdef FIBO
+long long int n;
+cout << "Введите предельное число: "; cin >> n;
+fibo(n);
+#endif
+
 }
+int factorial(int n)
+  {
+	if (n == 0) return 1;
+	else return n * factorial(n - 1);
+
+  }
 void elevator(int floor)
 {
 	if (floor == 0)
@@ -17,6 +52,21 @@ void elevator(int floor)
 		return;
 	}
 	cout << "Вы на  " << floor << "этаже\n";
-	elevator(floor-1);
+	elevator(floor - 1);
 	cout << "Вы на  " << floor << "этаже\n";
 }
+double power(int a, int n)
+{
+	if (n == 0)return 1;
+	else if (n > 0) return power(a, n - 1) * a;
+	else return 1 / power(a, -n);
+	return n == 0 ? 1 : n > 0 ? a * power(a, n - 1) : 1 / power(a, -n);
+}
+
+void fibo(long long int n, long long int a, long long int b)
+{
+	if (a > n)return;
+	cout << a << "\t";
+	fibo(n, b, a + b);
+}
+
